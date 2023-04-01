6d0750e295f252302d851f2f0a9aed5d40b74188
Pedro R. Borges <pedroR.borges@gMail.com>
2023-04-01 01:29:50 +0100

    Added error handling.

--- index.js
+++ index.js
@@ -12,18 +12,39 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const upload = multer({ limits: { fileSize: 5000000 } });
+const upload = multer({ limits: { fileSize: 5000000 } }).single("upfile");
 
 // Analyze the uploaded file
 // The file is kept in memory and limited to 5MB.
 // It could be saved in disk and then deleted.
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
-  res.status(200).json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size,
+app.post("/api/fileanalyse", (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.status(500).json({ error: "Error loading file" });
+      console.log("Multer error: ", err);
+      return;
+    }
+
+    if (err) {
+      res.status(500).json({ error: "Unknown error" });
+      console.log("Unknown error: ", err);
+      return;
+    }
+
+    if (!req.file) {
+      res.status(400).json({ error: "No file to upload" });
+      console.log("Bad request: no file to upload");
+      return;
+    }
+
+    res.status(200).json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
   });
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
